8098dcf0a74fbde51c990acc9ac56c0559a7bd8c
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2018-07-23 10:04:25 -0400

    Add 'y_new = y ∩ exp(x)' statements back into exponential definitions.

--- src/exponential.jl
+++ src/exponential.jl
@@ -11,35 +11,39 @@ doc"""
 Reverse function for `exp`.
 """
 function exp_rev(y::Interval, x::Interval)
+    y_new = y ∩ exp(x)
     x_new = x ∩ log(y)
-    return y, x_new
+    return y_new, x_new
 end
 
 doc"""
 Reverse function for `exp2`.
 """
 function exp2_rev(y::Interval, x::Interval)
+    y_new = y ∩ exp2(x)
     x_new = x ∩ log2(y)
 
-    return y, x_new
+    return y_new, x_new
 end
 
 doc"""
 Reverse function for `exp10`.
 """
 function exp10_rev(y::Interval, x::Interval)
+    y_new = y ∩ exp10(x)
     x_new = x ∩ log10(y)
 
-    return y, x_new
+    return y_new, x_new
 end
 
 doc"""
 Reverse function for `expm1`.
 """
 function expm1_rev(y::Interval, x::Interval)
+    y_new = y ∩ expm1(x)
     x_new = x ∩ log1p(y)
 
-    return y, x_new
+    return y_new, x_new
 end
 
 
