28ecaf2214a7878df951a154107a0b54a1ff47f6
David Sanders <dpsanders@gmail.com>
2024-07-14 19:16:20 -0500

    Add eq function for testing bareinterval equality

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using IntervalContractors
 using IntervalArithmetic
 using Test
 
+eq(a, b) = isequal_interval(bareinterval(a), bareinterval(b))
+
 @testset "IntervalContractors tests" begin
     include("abs_rev.jl")
     include("libieeep1788_rev.jl")
@@ -12,7 +14,7 @@ using Test
     @testset "power_rev for odd power" begin
         x = -∞..∞
         a = -8..27
-        power_rev(a, x, 3)[2] == interval(-2.0000000000000004, 3.0000000000000004)
+        @test eq(power_rev(a, x, 3)[2], interval(-2.0000000000000004, 3.0000000000000004))
     end
 
     @testset "mul_rev with sets straddling 0" begin
@@ -20,10 +22,10 @@ using Test
         y = -1..1
         z = 2..10
 
-        @test isequal_interval(mul_rev(z, x, y), (2..10, 2..5, 0.4..1.0))
+        @test isequal_interval.(mul_rev(z, x, y), (2..10, 2..5, 0.4..1.0))
     end
 
     @testset "Exponents with integer values but not types" begin
-        @test isequal_interval(power_rev(-Inf..Inf, -4..4, 2.0),  (-∞..∞, -4..4, 2.0))
+        @test isequal_interval.(power_rev(-Inf..Inf, -4..4, 2.0),  (-∞..∞, -4..4, 2.0))
     end
 end
